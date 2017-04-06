6a8b7f0e0e1904e0a954a094279058df7549de36
Kevin Wecker <kwecker@gmail.com>
2017-04-06 15:30:17 -0700

    First setup

---
+++ app/index.php
@@ -0,0 +1,49 @@
+<?php
+
+$section = 'Home';
+
+include('perch/runtime.php');
+perch_layout('global.header', array( 'section' => $section ));
+
+?>
+
+<section class="main-content">
+	<div class="row">
+		<section class="content nine columns">
+			<header>
+				<h1>Welcome to Marina Motorsports, Inc.</h1>
+			</header>
+			<section class="text" id="intro">
+				<p>
+					Marina Motorsports was first formed in 1995 with the partnership of five automotive enthusiasts. Over the years board members have come and gone, but three of the original founding members remain, Robert McCaffrey, John Moulton and David Murphy. Our fourth long standing board member is Ronald Cowen. Marina Motorsports became registered as a non-profit 501 (c) (4) corporation in 1996.
+				</p>
+				<p>
+					Marina Motorsports, Inc. is a non-profit organization. Our primary goal is to provide family oriented automotive related activities while benefiting local service clubs, other non-profit organizations and the City of Marina.
+				</p>
+				<p>
+					We patterned our by-laws and our operation after Sports Car Racing Association Monterey Peninsula. SCRAMP hosts major automotive and motorcycle events at Monterey county’s Laguna Seca. We have hosted smaller events such as go-kart racing, autocrossing, driving schools and automotive swap meets at Marina Municipal Airport, the former Fort Ord’s Fritsche Field. Our goals are the same in that we provide a motorsport venue to allow other service groups such the Boy and Girl Scouts, Kiwanis, Rotary, etc. to benefit. Our slogan says it all: <strong><em>“Wheels in Motion for Community Benefit”</em></strong>.
+				</p>
+			</section>
+
+			<section id="gallery">
+				<header>
+					<h2>The Little Car Show Gallery</h2>
+				</header>
+
+			</section>
+		</section>
+
+		<aside class="main-sidebar three columns">
+			<div class="button">
+				<img src="" alt="">
+				<span class="text">Free Swap Meets!</span>
+			</div>
+			<div class="button">
+				<img src="" alt="">
+				<span class="text">AutoX</span>
+			</div>
+		</aside>
+	</div>
+</section>
+
+<?php perch_layout('global.footer', array( 'section' => $section )); ?>
--- app/perch/templates/layouts/global.header.php
+++ app/perch/templates/layouts/global.header.php
@@ -8,8 +8,47 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?=$title?></title>
+
+	<!-- build:css /css/main.min.css -->
+	<link rel="stylesheet" href="/css/normalize.css">
+	<link rel="stylesheet" href="/css/skeleton.css">
+	<link rel="stylesheet" href="/css/main.css">
+	<!-- endbuild -->
 </head>
 <body>
 
-</body>
-</html>
+<div class="wrapper container">
+
+	<header class="main-header">
+		<nav id="main-navigation">
+			<ul class="menu menu--left">
+				<li class="menu__item"><a href="" class="menu__link">Home</a></li><!--
+			 --><li class="menu__item"><a href="" class="menu__link">Events</a></li><!--
+			 --><li class="menu__item"><a href="" class="menu__link">Calendar</a></li><!--
+			 --><li class="menu__item"><a href="" class="menu__link">Sponsors</a></li><!--
+			 --><li class="menu__item"><a href="" class="menu__link">Charities</a></li>
+			</ul>
+			<ul class="menu menu--right">
+				<li class="menu__item"><a href="" class="menu__link">Merchandise</a></li><!--
+			 --><li class="menu__item"><a href="" class="menu__link">Gallery</a></li><!--
+			 --><li class="menu__item"><a href="" class="menu__link">Media</a></li><!--
+			 --><li class="menu__item"><a href="" class="menu__link">Links</a></li><!--
+			 --><li class="menu__item"><a href="" class="menu__link">Contact Us</a></li>
+			</ul>
+		</nav>
+
+		<div class="masthead">
+			<span class="tagline tagline--left"><img src="" alt="Wheels in Motion"></span>
+			<div class="logo"><h1><img src="/images/logo.png" alt="Marina Motorsports"></h1></div>
+			<span class="tagline tagline--right"><img src="" alt="for Community Benefit"></span>
+		</div>
+	</header>
+
+	<section id="call-to-action">
+		<div class="row">
+			<section class="slideshow nine columns"></section>
+			<aside class="top-sidebar three columns"></aside>
+		</div>
+
+		<aside class="ticker"></aside>
+	</section>
