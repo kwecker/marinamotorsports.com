f98cc1c26d56caf96107d5242899ecfca0e7aced
Kevin Wecker <kwecker@gmail.com>
2017-04-26 18:45:09 -0700

    Add sidebar file

---
+++ app/parts/sidebar.php
@@ -0,0 +1,12 @@
+<aside class="sidebar sidebar--main">
+	<div class="wrap">
+		<div class="sidebar-button">
+			<img src="" alt="">
+			<span class="text">Free Swap Meets!</span>
+		</div>
+		<div class="sidebar-button">
+			<img src="" alt="">
+			<span class="text">AutoX</span>
+		</div>
+	</div>
+</aside>
