b4f7adbbba6484623d3171e9a6bcf05376819f1b
Kevin Wecker <kwecker@gmail.com>
2017-04-26 19:44:03 -0700

    Add charities page

---
+++ app/charities/index.php
@@ -0,0 +1,31 @@
+<?php
+$section = 'Charities';
+
+include('../perch/runtime.php');
+include('../parts/header.php');
+?>
+
+		<section class="content">
+			<div class="wrap">
+				<header>
+					<h1>Marina Motorsports, Inc. Charitable Beneficiaries</h1>
+				</header>
+				<section class="text" id="intro">
+					<p>
+						Charities Marina Motorsports, Inc. has donated to since 2005!
+					</p>
+					<ul class="multi-column bulletless charities">
+						<li><span class="name">Alliance on Aging</span><span class="cost">$2,000</span></li>
+						<li><span class="name">American Cancer Society</span><span class="cost">$2,500</span></li>
+						<li><span class="name">American Cancer Society (Sharon Taylor)</span><span class="cost">$1,950</span></li>
+						<li><span class="name">American Legion Post 694</span><span class="cost">$2,000</span></li>
+						<li><span class="name">American Friends Rescue Project</span><span class="cost">$500</span></li>
+					</ul>
+					<div class="charities-total">
+						<span>Total </span>$155,677.75
+					</div>
+				</section>
+			</div>
+		</section>
+
+<?php include('../parts/footer.php'); ?>
